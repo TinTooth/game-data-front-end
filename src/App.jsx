339585ab720ce1eb747430e570e3d9183f0f13f7
TinTooth <boconne4@gmail.com>
2022-11-29 13:16:05 -0600

    Created Search Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-
+import axios from 'axios';
 import './App.css';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
+import { useState, useEffect } from 'react';
+import SearchContainer from './Components/SearchContainer/SearchContainer';
 
 function App() {
+  const [data, setData] = useState();
+  const [game, setGame] = useState(null);
+
+  useEffect (()=> {
+    getData();
+  },[])
+
+  async function getData(){
+    const response = await axios.get("http://localhost:8080/all/");
+    setData(response.data);
+  }
+
   return (
     <div className="App">
      <Header/>
+     <SearchContainer data={data} setGame ={setGame}/>
      <Footer/>
     </div>
   );
---
+++ src/Components/SearchContainer/SearchContainer.jsx
@@ -0,0 +1,12 @@
+import "./SearchContainer.css";
+
+const SearchContainer = ({data,setGame}) => {
+    return (
+        <div className="search-container">
+            test
+
+        </div>
+      );
+}
+ 
+export default SearchContainer;
