ac6b9478ca87ff7447e88ed9b4214e938a1c5e4f
TinTooth <boconne4@gmail.com>
2022-11-29 12:18:42 -0600

    Created Header Component + Styling

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+const Header = () => {
+    return (  
+        <div className="header">
+           <div className="brand">Game Data</div> 
+        </div>
+    
+    );
+}
+ 
+export default Header;
