4a2d7aa611c9cfb4b3cd29bbc151b06d68a388e7
TinTooth <boconne4@gmail.com>
2022-11-29 14:42:03 -0600

    Added Filter and Sort Functions to Search functionality

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -9,9 +9,20 @@ const SearchBar = ({data, setResults}) => {
 
     const handleSubmit = (e) =>{
         e.preventDefault();
-        let results = data.filter((game) => {return game.name.toLowerCase().includes(search.toLocaleLowerCase())})
-        setResults(results);
+        let results = data.filter((game) => {return game.name.toLowerCase().includes(search.toLocaleLowerCase())});
+        let filteredGames =removeDuplicates(results);
+        let sorted = filteredGames.sort((g1, g2) => (g1.name.toLocaleLowerCase() > g2.name.toLocaleLowerCase()) ? 1 : (g1.name.toLocaleLowerCase() < g2.name.toLocaleLowerCase()) ? -1 : 0);
+        setResults(sorted);
     }
+
+    const removeDuplicates = (games) =>{
+    
+        const filteredGames = games.filter((game,i,games) => 
+        i === games.findIndex((g) => (
+            g.name ===game.name)))
+        return filteredGames;
+    }
+
 
     const handleClear = () =>{
         setSearch("");
