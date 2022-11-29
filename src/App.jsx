755092490215a2619dfe28223c6a1ecb67bbb5b4
TinTooth <boconne4@gmail.com>
2022-11-29 12:23:41 -0600

    Created Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Footer/>
     </div>
   );
 }
